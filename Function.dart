e7ec70fa3e2847446b90bf5ef182bc8fb5a521de
peerapattop <peerapatza401@gmail.com>
2023-07-05 12:14:13 +0700

    Update 5/7/2566

---
+++ Function.dart
@@ -0,0 +1,13 @@
+// (14) การสร้างฟังก์ชัน
+
+void main(){
+
+  addNumber(10, 10);
+}
+
+void show(var message){
+  print("Hello $message");
+}
+void addNumber(var num1,var num2){
+  print("$num1 + $num2 = ${num1+num2}");
+}
