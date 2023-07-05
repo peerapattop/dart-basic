0cb973da97bf886945139d1789f6d13dbf737a72
peerapattop <peerapatza401@gmail.com>
2023-07-05 11:14:38 +0700

    Update 5/7/2566

---
+++ TernaryOperator.dart
@@ -0,0 +1,10 @@
+// (13) Ternary Operator
+
+void main(){
+  var number1 = 500,number2 = 200;
+  String result;
+  //            เงื่อนไข       ? จริง                               : เท็จ
+  result = (number1>number2) ? result ="number1 มากกว่า number2" : "number1 น้อยกว่า number2";
+  print(result);
+
+}
